ea95dbe6ef9d21ff0be2031d48c93d4ed3553d51
Tobias Köhler <29401795+ToKoel@users.noreply.github.com>
2026-06-06 12:46:58 +0200

    fix treesitter

--- init.lua
+++ init.lua
@@ -897,9 +897,14 @@ do
   -- NOTE: You can also specify a branch or a specific commit
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
-  -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
-  require('nvim-treesitter').install(parsers)
+  local ts = require('nvim-treesitter')
+
+  ts.setup({
+    ensure_installed = {
+      'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      sync_install = false,
+      auto_install = true,
+    })
 
   ---@param buf integer
   ---@param language string
@@ -922,26 +927,14 @@ do
     if has_indent_query then vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" end
   end
 
-  local available_parsers = require('nvim-treesitter').get_available()
   vim.api.nvim_create_autocmd('FileType', {
     callback = function(args)
       local buf, filetype = args.buf, args.match
 
-      local language = vim.treesitter.language.get_lang(filetype)
-      if not language then return end
+      local language = vim.treesitter.language.get_lang(filetype) or filetype
 
-      local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
 
-      if vim.tbl_contains(installed_parsers, language) then
-        -- Enable the parser if it is already installed
         treesitter_try_attach(buf, language)
-      elseif vim.tbl_contains(available_parsers, language) then
-        -- If a parser is available in `nvim-treesitter`, auto-install it and enable it after the installation is done
-        require('nvim-treesitter').install(language):await(function() treesitter_try_attach(buf, language) end)
-      else
-        -- Try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
-        treesitter_try_attach(buf, language)
-      end
     end,
   })
 end
